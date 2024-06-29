0d3b1c6a9d42722b2422092cffd9c3bd98e52760
Shahabuddin1122 <shavoddin54@gmail.com>
2024-06-30 02:05:53 +0600

    update landing page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -4,19 +4,26 @@ import Header from "@/components/header";
 import Footer from "@/components/footer";
 import Dropdown from "@/components/DropDown";
 import LandingHeader from "@/components/landingHeader";
+import RoomSuitesComponent from "@/components/RoomSuitesComponent";
 export default function Home() {
   return (
       <>
-          <LandingHeader />
-          <div className={"w-11/12 h-[600px] my-10 flex"}>
+          <LandingHeader/>
+          <div className={"w-11/12 h-[500px] my-10 flex"}>
               <div className={"w-1/2 h-full bg-gray-200 relative"}>
-                  <Image src={"/restaurant2.jpg"} alt={"restaurant"} fill objectFit={"cover"} />
+                  <Image src={"/restaurant2.jpg"} alt={"restaurant"} fill objectFit={"cover"}/>
               </div>
               <div className={"w-1/2 h-full flex justify-end items-center"}>
                   <div className={"w-4/5 h-4/5"}>
-                      <h1 className={"text-3xl font-extrabold tracking-wide"}>Ordinary Best Western 5 Stars Hotel Since 1998</h1>
-                      <p className={"py-4"}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Volutpat elit sed pretium, egestas sed sit. Fames tincidunt rhoncus viverra eu ut scelerisque. Erat orci scelerisque adipiscing potenti sollicitudin semper aliquam in ultricies. Sem vitae amet, egestas aliquam mi a arcu. Feugiat at dignissim massa ornare. Platea eu orci enim est egestas fusce cras.   Purus diam est vitae faucibus enim. Ultricies nunc vel magnis gravida quis sodales. Lacus, elit pellentesque massa odio. Sed dictumst condimentum sit quis </p>
-                      <Button text={"Learn more"} />
+                      <h1 className={"text-3xl font-extrabold tracking-wide"}>Ordinary Best Western 5 Stars Hotel Since
+                          1998</h1>
+                      <p className={"py-4"}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Volutpat elit sed
+                          pretium, egestas sed sit. Fames tincidunt rhoncus viverra eu ut scelerisque. Erat orci
+                          scelerisque adipiscing potenti sollicitudin semper aliquam in ultricies. Sem vitae amet,
+                          egestas aliquam mi a arcu. Feugiat at dignissim massa ornare. Platea eu orci enim est egestas
+                          fusce cras. Purus diam est vitae faucibus enim. Ultricies nunc vel magnis gravida quis
+                          sodales. Lacus, elit pellentesque massa odio. Sed dictumst condimentum sit quis </p>
+                      <Button text={"Learn more"}/>
                   </div>
               </div>
           </div>
@@ -39,11 +46,12 @@ export default function Home() {
                   </div>
               </div>
           </div>
-          <div className={"w-full px-[50px] border"}>
+          <div className={"w-full px-[50px]"}>
               <div className={"w-full flex justify-between items-center"}>
                   <div className={"w-1/3"}>
-                      <p className={"font-extrabold text-xl pb-2"}>Our Room & Suites</p>
-                      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Volutpat elit sed pretium, egestas sed sit.</p>
+                      <p className={"font-extrabold text-xl pb-3"}>Our Room & Suites</p>
+                      <p className={"pb-2"}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Volutpat elit sed
+                          pretium, egestas sed sit.</p>
                   </div>
                   <div className={"flex gap-2"}>
                       <div className={"border-4 border-primary rounded px-3 py-2"}>
@@ -54,11 +62,161 @@ export default function Home() {
                       </div>
                   </div>
               </div>
-              <div className={"w-full h-[200px] my-6 border border-primary"}>
-
+              <div className={"my-6 relative overflow-hidden"}>
+                  <div className={"flex flex-row overflow-x-auto no-scrollbar"}>
+                      <div className={"flex"}>
+                          <RoomSuitesComponent header={"Deluxe Room"} image={"/hotel.jpg"}/>
+                          <RoomSuitesComponent header={"Standard Room"} image={"/room1.jpg"}/>
+                          <RoomSuitesComponent header={"Premium Room"} image={"/room2.jpg"}/>
+                          <RoomSuitesComponent header={"Family Room"} image={"/room3.jpg"}/>
+                          <RoomSuitesComponent header={"Honeymoon Room"} image={"/room4.jpg"}/>
+                      </div>
+                  </div>
               </div>
-
           </div>
+          <div className={"w-full h-[500px] my-10  flex justify-end"}>
+              <div className={"w-11/12 h-full flex"}>
+                  <div className={"w-1/2 h-full flex items-center"}>
+                      <div className={"w-4/5 h-4/5"}>
+                          <h1 className={"text-3xl font-bold tracking-wider py-5"}>La Vie Restaurant</h1>
+                          <p className={"text-sm tracking-wide text-gray-400"}>Lorem ipsum dolor sit amet, consectetur
+                              adipiscing elit. Volutpat elit sed pretium, egestas sed sit. Fames tincidunt rhoncus
+                              viverra eu ut scelerisque. Erat orci scelerisque adipiscing potenti sollicitudin semper
+                              aliquam in ultricies. Sem vitae amet, egestas aliquam mi a arcu. Feugiat at dignissim
+                              massa ornare. Platea eu orci enim est egestas fusce cras.</p>
+                          <div className={"py-5 text-gray-400 flex flex-col gap-3"}>
+                              <p>Vienna Bar</p>
+                              <p>Rolio Club</p>
+                              <p>In Room Dinning</p>
+                          </div>
+                          <Button text={"Learn more"}/>
+                      </div>
+                  </div>
+                  <div className={"w-1/2 h-full bg-gray-400 relative"}>
+                      <Image src={"/restaurant4.jpg"} alt={"restaurant"} fill objectFit={"cover"}/>
+                  </div>
+              </div>
+          </div>
+          <div className={"w-11/12 h-[500px] my-10 flex"}>
+              <div className={"w-1/2 h-full relative"}>
+                  <Image src={"/restaurant3.jpg"} alt={"restaurant"} fill objectFit={"cover"}/>
+              </div>
+              <div className={"w-1/2 h-full flex justify-end items-center"}>
+                  <div className={"w-4/5 h-4/5"}>
+                      <h1 className={"text-3xl font-extrabold tracking-wide"}>Jazucci Private Spa</h1>
+                      <p className={"py-4"}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Volutpat elit sed
+                          pretium, egestas sed sit. Fames tincidunt rhoncus viverra eu ut scelerisque. Erat orci
+                          scelerisque adipiscing potenti sollicitudin semper aliquam in ultricies. Sem vitae amet,
+                          egestas aliquam mi a arcu. Feugiat at dignissim massa ornare. Platea eu orci enim est egestas
+                          fusce cras.</p>
+                      <p className={"pb-3"}>Purus diam est vitae faucibus enim. Ultricies nunc vel magnis gravida quis
+                          sodales. Lacus, elit pellentesque massa odio. Sed dictumst condimentum sit quis </p>
+                      <Button text={"Learn more"}/>
+                  </div>
+              </div>
+          </div>
+          <div className={"w-full h-[500px] my-10  flex justify-end"}>
+              <div className={"w-11/12 h-full flex"}>
+                  <div className={"w-1/2 h-full flex items-center"}>
+                      <div className={"w-4/5 h-4/5"}>
+                          <h1 className={"text-3xl font-extrabold tracking-wide"}>Ocenia Waterpool</h1>
+                          <p className={"py-4"}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Volutpat elit
+                              sed pretium, egestas sed sit. Fames tincidunt rhoncus viverra eu ut scelerisque. Erat orci
+                              scelerisque adipiscing potenti sollicitudin semper aliquam in ultricies. Sem vitae amet,
+                              egestas aliquam mi a arcu. Feugiat at dignissim massa ornare. Platea eu orci enim est
+                              egestas fusce cras. </p>
+                          <p className={"pb-3"}>Purus diam est vitae faucibus enim. Ultricies nunc vel magnis gravida
+                              quis sodales. Lacus, elit pellentesque massa odio. Sed dictumst condimentum sit quis </p>
+                          <Button text={"Learn more"}/>
+                      </div>
+                  </div>
+                  <div className={"w-1/2 h-full bg-gray-400 relative"}>
+                      <Image src={"/restaurant4.jpg"} alt={"restaurant"} fill objectFit={"cover"}/>
+                  </div>
+              </div>
+          </div>
+          <div className={"w-full h-[280px]"}></div>
+          <div className={"w-full h-[320px] bg-secondary flex justify-center"}>
+              <div className={"w-3/5 h-[500px] relative -top-[250px]"}>
+                  <Image src={"/map.png"} alt={"hotel"} fill objectFit={"cover"} />
+              </div>
+          </div>
+          <div className={"w-full h-[500px] py-10 bg-secondary flex gap-2 flex-col"}>
+              <div className={"w-full flex  justify-center"}>
+                  <div className={"w-11/12 h-1/4 flex justify-between items-center"}>
+                      <div className={"w-1/2"}>
+                          <h1 className={"text-white text-2xl font-bold tracking-widest pb-4"}>Our Gallery</h1>
+                          <p className={"text-white"}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Volutpat
+                              elit sed pretium, egestas sed sit.</p>
+                      </div>
+                      <Button text={"Open Instagram"}/>
+                  </div>
+              </div>
+              <div className={"w-full h-full flex relative overflow-hidden"}>
+                  <div className={"flex overflow-x-auto no-scrollbar"}>
+                      <div className={"flex gap-3"}>
+                          <div className={"w-[220px] h-full bg-slate-500 relative"}>
+                              <Image src={"/gallery1.jpg"} alt={"photo"} fill objectFit={"cover"} />
+                          </div>
+                          <div className={"w-[220px] h-full flex flex-col gap-3"}>
+                              <div className={"w-full h-1/3 bg-slate-500 relative"}>
+                                  <Image src={"/gallery2.jpg"} alt={"photo"} fill objectFit={"cover"} />
+                              </div>
+                              <div className={"w-full h-2/3 bg-slate-500 relative"}>
+                                  <Image src={"/gallery3.jpg"} alt={"photo"} fill objectFit={"cover"} />
+                              </div>
+                          </div>
+                          <div className={"w-[220px] h-full flex flex-col gap-3"}>
+                              <div className={"w-full h-1/2 bg-slate-500 relative"}>
+                                  <Image src={"/gallery4.jpg"} alt={"photo"} fill objectFit={"cover"} />
+                              </div>
+                              <div className={"w-full h-1/2 bg-slate-500 relative"}>
+                                  <Image src={"/gallery5.jpg"} alt={"photo"} fill objectFit={"cover"} />
+                              </div>
+                          </div>
+                          <div className={"w-[220px] h-full bg-slate-500 relative"}>
+                              <Image src={"/gallery6.jpg"} alt={"photo"} fill objectFit={"cover"} />
+                          </div>
+                          <div className={"w-[220px] h-full flex flex-col gap-3"}>
+                              <div className={"w-full h-1/2 bg-slate-500 relative"}>
+                                  <Image src={"/gallery2.jpg"} alt={"photo"} fill objectFit={"cover"} />
+                              </div>
+                              <div className={"w-full h-1/2 bg-slate-500 relative"}>
+                                  <Image src={"/gallery11.jpg"} alt={"photo"} fill objectFit={"cover"} />
+                              </div>
+                          </div>
+                          <div className={"w-[220px] h-full flex flex-col gap-3"}>
+                              <div className={"w-full h-3/4 bg-slate-500 relative"}>
+                                  <Image src={"/gallery7.jpg"} alt={"photo"} fill objectFit={"cover"} />
+                              </div>
+                              <div className={"w-full h-1/4 bg-slate-500 relative"}>
+                                  <Image src={"/gallery3.jpg"} alt={"photo"} fill objectFit={"cover"} />
+                              </div>
+                          </div>
+                          <div className={"w-[220px] h-full flex flex-col gap-3"}>
+                              <div className={"w-full h-1/5 bg-slate-500 relative"}>
+                                  <Image src={"/gallery5.jpg"} alt={"photo"} fill objectFit={"cover"} />
+                              </div>
+                              <div className={"w-full h-4/5 bg-slate-500 relative"}>
+                                  <Image src={"/gallery8.jpg"} alt={"photo"} fill objectFit={"cover"} />
+                              </div>
+                          </div>
+                          <div className={"w-[220px] h-full bg-slate-500 relative"}>
+                              <Image src={"/gallery9.jpg"} alt={"photo"} fill objectFit={"cover"} />
+                          </div>
+                          <div className={"w-[220px] h-full flex flex-col gap-3"}>
+                              <div className={"w-full h-1/2 bg-slate-500 relative"}>
+                                  <Image src={"/gallery10.jpg"} alt={"photo"} fill objectFit={"cover"} />
+                              </div>
+                              <div className={"w-full h-1/2 bg-slate-500 relative"}>
+                                  <Image src={"/gallery11.jpg"} alt={"photo"} fill objectFit={"cover"} />
+                              </div>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+          </div>
+          <Footer/>
       </>
   );
 }
