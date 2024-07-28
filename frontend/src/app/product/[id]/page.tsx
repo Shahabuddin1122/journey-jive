b12d4fba8d94747c3cd35f443ff4e5b1d178276d
Shahabuddin1122 <shavoddin54@gmail.com>
2024-07-29 05:49:54 +0600

    add calendar and connected with individual product api

--- frontend/src/app/product/[id]/page.tsx
+++ frontend/src/app/product/[id]/page.tsx
@@ -1,14 +1,34 @@
+"use client"
 import Image from "next/image";
 import Header from "@/components/header";
 import Button from "@/components/Button";
 import IconText from "@/components/IconText";
 import Footer from "@/components/footer";
 import Dropdown from "@/components/DropDown";
-import DateTimePicker from 'react-datetime-picker';
-import DatePicker from "react-date-picker";
 import RoomComponent from "@/components/RoomComponent";
+import {useEffect, useState} from "react";
+import axios from "axios";
+import Calender from "@/components/calender";
+import {da} from "date-fns/locale";
 
-const product = () => {
+const Product = ({params}) => {
+    const productId = params.id;
+    const [data,setData] = useState<Hotel>(null)
+    const [selectedDate,setSelectedDate] = useState<string>('')
+
+    const dateSelector = (date)=>{
+        setSelectedDate(date);
+    }
+
+    useEffect(() => {
+        axios.get(`http://localhost:8000/hotel/get-indivisual-hotel/${productId}`)
+            .then((res)=>{
+                setData(res.data)
+            })
+            .catch((err)=>{
+                console.error(err);
+            })
+    }, [data]);
     return (
         <>
             <div>
@@ -17,53 +37,44 @@ const product = () => {
                         <div className={"w-4/5 h-1/2 my-auto flex flex-col justify-between"}>
                             <div className={"w-full h-full flex flex-col justify-around"}>
                                 <div>
-                                    <h1 className={"text-5xl font-bold tracking-wide"}>President Luxury Double Room View
-                                        NY
-                                        City </h1>
-                                    <p className={"text-sm py-4"}>Lorem ipsum dolor sit amet, consectetur adipiscing
-                                        elit.
-                                        Volutpat
-                                        elit sed pretium, egestas sed sit orem ipsum dolor sit amet, lorem ghata udbjcbd
-                                        hjdcj
-                                        dkjhr nxn orem jxbi kxe</p>
+                                    <h1 className={"text-5xl font-bold tracking-wide"}>{data && data.name}</h1>
+                                    <p className={"text-sm py-4"}>{data && data.short_description}</p>
                                 </div>
                                 <div className={"flex gap-16"}>
                                     <div className={"flex gap-2 justify-center items-center"}>
                                         <Image src={"/persons.svg"} alt={"search"} height={20} width={20}/>
-                                        <p className={"text-slate-400"}>2 Guest</p>
+                                        <p className={"text-slate-400"}>{data && data.guest_per_room} Guest</p>
                                     </div>
                                     <div className={"flex gap-2 justify-center items-center"}>
                                     <Image src={"/bed.svg"} alt={"search"} height={20} width={20}/>
-                                        <p className={"text-slate-400"}>2 Bed</p>
+                                        <p className={"text-slate-400"}>{data && data.bed_per_room} Bed</p>
                                     </div>
                                     <div className={"flex gap-2 justify-center items-center"}>
                                         <Image src={"/washroom.svg"} alt={"search"} height={20} width={20}/>
-                                        <p className={"text-slate-400"}>1 Washroom</p>
+                                        <p className={"text-slate-400"}>{data && data.washroom_per_room} Washroom</p>
                                     </div>
                                 </div>
                                 <div className={"pt-10"}>
                                     <p className={"text-secondary"}>Start From</p>
-                                    <p className={"text-4xl font-bold"}>$59<span
+                                    <p className={"text-4xl font-bold"}>${data && data.price_per_room}<span
                                         className={"text-lg font-light text-secondary"}>/Night</span></p>
                                 </div>
                             </div>
                         </div>
                     </div>
                     <div className={"w-[43%] h-full bg-slate-100 absolute right-0"}>
-                        <Image src={"/hotel.jpg"} alt={"home"} fill objectFit={"cover"}/>
+                        <Image src={`data:image/jpeg;base64,${data && data.hotel_image.heroImage}`} alt={"home"} fill objectFit={"cover"}/>
                     </div>
                 </div>
                 <Header clickedItem={1}/>
                 <div className={"w-11/12 h-[500px] my-10 flex"}>
                     <div className={"w-1/2 h-full bg-gray-200 relative"}>
-                        <Image src={"/restaurant2.jpg"} alt={"restaurant"} fill objectFit={"cover"}/>
+                        <Image src={`data:image/jpeg;base64,${data && data.hotel_image.additionalImage}`} alt={"restaurant"} fill objectFit={"cover"}/>
                     </div>
                     <div className={"w-1/2 h-full flex justify-end items-center"}>
                         <div className={"w-4/5 h-4/5"}>
-                            <h1 className={"text-3xl font-extrabold tracking-wide"}>About President Luxury Double Room View NY City </h1>
-                            <p className={"pt-2"}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Volutpat elit sed pretium, egestas sed sit. Fames tincidunt rhoncus viverra eu ut scelerisque. Erat orci scelerisque adipiscing potenti sollicitudin semper aliquam in ultricies. Sem vitae amet, egestas aliquam mi a arcu.</p>
-                            <p className={"pt-2"}>Purus diam est vitae faucibus enim. Ultricies nunc vel magnis  massa odio. Sed dictumst condimentum sit quis  Lorem ipsum dolor sit egestas sed sit. Fames tincidunt rhoncus viverra eu ut sce scelerisque adipiscing potenti  in ultricies. Sem vitae amet, egestas aliquam mi a arcu. Feugiat at dignissim massa ornare. Platea eu orci enim est egestas fusce cras.</p>
-                            <p className={"pt-2"}>Purus diam est vitae faucibus enim. Ultricies nunc vel magnis gravida quis sodales. Lacus, elit pellentesque massa odio. Sed dictumst condimentum sit quis.</p>
+                            <h1 className={"text-3xl font-extrabold tracking-wide"}>About {data && data.name}</h1>
+                            <p className={"pt-2"}>{data && data.about}</p>
                         </div>
                     </div>
                 </div>
@@ -72,7 +83,7 @@ const product = () => {
                         <h1 className={"text-2xl font-semibold"}>Hotels Facilities</h1>
                     </div>
                     <div className={"w-1/3"}>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Volutpat elit sed pretium, egestas sed sit.</p>
+                        <p>Our hotel, {data && data.name} provide many more facilities including</p>
                     </div>
                     <div className={"w-full h-full my-5 flex flex-wrap"}>
                         <div className={"w-1/3 h-10"}>
@@ -138,17 +149,9 @@ const product = () => {
                     <div className={"w-1/2 h-full flex justify-center flex-col"}>
                         <div className={"w-4/5 flex gap-4 flex-col justify-center"}>
                             <h1 className={"text-2xl font-semibold pb-4"}>Additional Information</h1>
-                            <p className={"text-sm"}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Volutpat
-                                elit sed pretium, egestas sed sit. Fames tincidunt rhoncus viverra eu ut scelerisque.
-                                Erat orci scelerisque adipiscing potenti sollicitudin semper aliquam in ultricies. Sem
-                                vitae amet, egestas aliquam mi a arcu.</p>
-                            <p className={"text-sm"}>Purus diam est vitae faucibus enim. Ultricies nunc vel magnis massa
-                                odio. Sed dictumst condimentum sit quis Lorem ipsum dolor sit egestas sed sit. Fames
-                                tincidunt rhoncus viverra eu ut sce scelerisque adipiscing potenti in ultricies. Sem
-                                vitae amet, egestas aliquam mi a arcu. Feugiat at dignissim massa ornare. Platea eu orci
-                                enim est egestas fusce cras.</p>
-                            <h2 className={"text-lg font-semibold"}>Check in 15:00 - 23:59</h2>
-                            <h2 className={"text-lg font-semibold"}>Check Out 12:00</h2>
+                            <p className={"text-sm"}>{data && data.additional_info}</p>
+                            <h2 className={"text-lg font-semibold"}>Check in {data && data.check_in}</h2>
+                            <h2 className={"text-lg font-semibold"}>Check Out {data && data.check_out}</h2>
                         </div>
                     </div>
                     <div className={"w-1/2 h-full flex justify-center items-center"}>
@@ -164,23 +167,24 @@ const product = () => {
                 <div className={"w-[94%] mb-5 float-right "}>
                     <div className={"w-1/2 flex flex-col gap-4"}>
                         <h1 className={"text-2xl font-semibold"}>Room Availability</h1>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Volutpat elit sed pretium, egestas sed sit.</p>
+                        <p>Please select date from calender and additional information of your bookings.</p>
                     </div>
                     <div className={"w-full my-5 flex justify-center items-center gap-4"}>
                         <div className={"w-1/2 h-[340px] border-2 rounded-xl flex justify-center items-center"}>
                             {/*/* calender*/}
+                            <Calender dateSelector={dateSelector}/>
+
                         </div>
                         <div className={"w-1/2 h-full border-2 rounded-xl flex justify-center items-center "}>
                             <div className={"w-[95%] pt-10 pb-5"}>
-                                <p className={"border-b-2 pb-2"}>Sellected Date :<span className={"font-semibold"}>14 February 2021</span>
+                                <p className={"border-b-2 pb-2"}>Sellected Date : <span className={"font-semibold"}>{selectedDate}</span>
                                 </p>
-                                <h1 className={"text-xl font-semibold py-2"}>President Luxury Double Room View NY
-                                    City </h1>
+                                <h1 className={"text-xl font-semibold py-2"}>{data && data.name}</h1>
                                 <p>Room left</p>
-                                <p className={"text-primary text-xl"}>52 Room Left</p>
+                                <p className={"text-primary text-xl"}>{data && data.total_rooms} Room Left</p>
                                 <div className={"py-5"}>
                                     <p className={"text-secondary"}>Start From</p>
-                                    <p className={"text-4xl font-bold"}>$59<span
+                                    <p className={"text-4xl font-bold"}>${data && data.price_per_room}<span
                                         className={"text-lg font-light text-secondary"}>/Night</span></p>
                                 </div>
                                 <Button route={'/checkout'} text={"Booking now"} />
@@ -194,10 +198,10 @@ const product = () => {
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Volutpat elit sed pretium, egestas sed sit.</p>
                     </div>
                     <div className={"w-full my-5 flex justify-center items-center gap-4"}>
-                        <RoomComponent option={1} image={"/restaurant4.jpg"} text={"Deluxe Double with New York City View"} price={'$67'} bed={"2"} guest={"3"} washroom={"2"} />
-                        <RoomComponent option={1} image={"/restaurant2.jpg"} text={"Deluxe Double with New York City View"} price={'$59'} bed={"2"} guest={"2"} washroom={"1"} />
-                        <RoomComponent option={1} image={"/restaurant3.jpg"} text={"Deluxe Double with New York City View"} price={'$20'} bed={"1"} guest={"1"} washroom={"1"} />
-                        <RoomComponent option={1} image={"/restaurant3.jpg"} text={"Deluxe Double with New York City View"} price={'$102'} bed={"3"} guest={"5"} washroom={"2"} />
+                        <RoomComponent option={1} image={"/restaurant4.jpg"} text={"Deluxe Double with New York City View"} price={67} bed={2} guest={3} washroom={2} />
+                        <RoomComponent option={1} image={"/restaurant2.jpg"} text={"Deluxe Double with New York City View"} price={59} bed={2} guest={2} washroom={1} />
+                        <RoomComponent option={1} image={"/restaurant3.jpg"} text={"Deluxe Double with New York City View"} price={20} bed={1} guest={1} washroom={1} />
+                        <RoomComponent option={1} image={"/restaurant3.jpg"} text={"Deluxe Double with New York City View"} price={102} bed={3} guest={5} washroom={2} />
                     </div>
                 </div>
 
@@ -206,4 +210,4 @@ const product = () => {
         </>
     )
 }
-export default product;
+export default Product;
