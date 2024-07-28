1a9de0be787eca413e7e04664cdbf4f80b1ee093
Shahabuddin1122 <shavoddin54@gmail.com>
2024-07-29 02:22:19 +0600

    add individual product api

--- backend/hotel/urls.py
+++ backend/hotel/urls.py
@@ -4,5 +4,6 @@ from hotel import views
 
 urlpatterns = [
     path('get-all-hotel', views.get_all_hotel),
-    path('add-hotel', views.add_hotel)
+    path('add-hotel', views.add_hotel),
+    path('get-indivisual-hotel/<int:hotel_id>', views.get_individual_hotel)
 ]
